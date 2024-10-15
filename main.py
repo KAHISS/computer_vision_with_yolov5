fd37bf78388bcc38e8499587c91dcd8b029510bb
Kaique Silva Sousa <ksilvasousa1@gmail.com>
2024-10-15 14:00:49 -0300

    adicionando pasta para arquivos teste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from webcam import WebCam
 from detectors import model, detect_car # importe só o modelo e as funções que vai usar
 
-a = WebCam(0, model)
-a.callWebcam(lambda: detect_car(2, a.frame), False)
+a = WebCam('data/walking.mp4', model)
+a.callWebcam(lambda: detect_car(a.frame), False)
